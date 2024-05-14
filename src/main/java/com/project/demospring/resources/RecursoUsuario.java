79c668c280f61a2bb1a350c5d9088aada8b49e5d
SouErick <ericksousasaraiva@gmail.com>
2024-05-14 17:23:54 -0300

    Implementando camada serviços

--- src/main/java/com/project/demospring/resources/RecursoUsuario.java
+++ src/main/java/com/project/demospring/resources/RecursoUsuario.java
@@ -1,18 +1,30 @@
 package com.project.demospring.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.demospring.entities.Usuario;
+import com.project.demospring.services.ServicosUsuario;
 
 @RestController
 @RequestMapping(value = "/usuarios")
 public class RecursoUsuario {
+	@Autowired
+	private ServicosUsuario servico;
 	@GetMapping
-	public ResponseEntity<Usuario> acharTodos(){
-		Usuario u = new Usuario(1L, "Marcos", "marcos@gmail.com", "9999999", "123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<Usuario>> acharTodos(){
+		List<Usuario> lista = servico.acharTodos();
+		return ResponseEntity.ok().body(lista);
+	}
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Usuario> acharPorId(@PathVariable Long id){
+		Usuario obj = servico.acharPorId(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/project/demospring/services/ServicosUsuario.java
@@ -0,0 +1,22 @@
+package com.project.demospring.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.project.demospring.entities.Usuario;
+import com.project.demospring.repositories.RepositorioUsuario;
+@Service
+public class ServicosUsuario {
+	@Autowired
+	private RepositorioUsuario repositorio;
+	public List<Usuario> acharTodos(){
+		return repositorio.findAll();
+	}
+	public Usuario acharPorId(Long id) {
+		Optional<Usuario> obj = repositorio.findById(id);
+		return obj.get();
+	}
+}
