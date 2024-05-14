5c3746cfba8bf6e28b29b4ca9aa24428dd8f9e6d
SouErick <ericksousasaraiva@gmail.com>
2024-05-14 16:48:12 -0300

    Implementação de configurações do banco de dados

--- src/main/java/com/project/demospring/entities/Usuario.java
+++ src/main/java/com/project/demospring/entities/Usuario.java
@@ -3,8 +3,15 @@ package com.project.demospring.entities;
 import java.io.Serializable;
 import java.util.Objects;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+@Entity
 public class Usuario implements Serializable{
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
